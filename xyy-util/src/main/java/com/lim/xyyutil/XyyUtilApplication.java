de6ae9f62a5fa52870ea22b57e0ccddccfb9dc10
528032307@qq.com <qh960128>
2018-11-19 17:46:05 +0800

    initial commit

---
+++ xyy-util/src/main/java/com/lim/xyyutil/XyyUtilApplication.java
@@ -0,0 +1,12 @@
+package com.lim.xyyutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyyUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XyyUtilApplication.class, args);
+    }
+}
---
+++ xyy-util/src/test/java/com/lim/xyyutil/XyyUtilApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lim.xyyutil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XyyUtilApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
