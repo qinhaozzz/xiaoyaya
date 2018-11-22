e6dc6ac5f4f66844cac9922a2865e2e46c8550bf
528032307@qq.com <qh960128>
2018-11-22 18:05:03 +0800

    悲观锁/乐观锁

---
+++ xyy-util/src/main/java/com/lim/xyyutil/test/AtomicTest.java
@@ -0,0 +1,18 @@
+package com.lim.xyyutil.test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 原子类
+ * @author qinhao
+ */
+public class AtomicTest {
+
+
+    public static void main(String[] args) {
+        AtomicInteger integer = new AtomicInteger();
+        System.out.println(integer);
+    }
+
+
+}
